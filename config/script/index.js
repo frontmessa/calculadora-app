d971622011838a7f8c825a83809fd0f8b9434889
frontmessa <caio-messa@hotmail.com>
2023-09-25 15:51:58 -0300

    funcionalidades dos botoes de numero

---
+++ config/script/index.js
@@ -0,0 +1,69 @@
+console.log("embedou");
+
+let visorEL;
+let buttonsEls;
+
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+function init(){
+
+    setElements();
+   
+
+
+}
+function setElements(){
+
+    visorEL = document.querySelector("#display>span");
+    buttonsEls =  Array.from(document.querySelectorAll("#buttonsContainer>button"));
+
+    addOnClickOnButtons();
+
+}
+
+function addOnClickOnButtons (){
+
+    console.log(buttonsEls.length);
+    console.log(buttonsEls[0]);
+
+
+
+    for(let i = 0; i < buttonsEls.length; i++){
+
+        buttonsEls[i].onclick = handleOnClickOnButtons;
+
+
+
+    }
+
+ 
+
+}
+
+function handleOnClickOnButtons(event){
+
+    const buttonValue = event.target.innerText;
+    const convertedButtonValue = parseInt(buttonValue);
+    const buttonValueIsNumber = !isNaN(convertedButtonValue);
+
+    if(buttonValueIsNumber === true){
+        writeOnVisor(buttonValue);
+
+
+     
+
+    }
+
+    
+
+
+}
+
+
+
+function writeOnVisor(_digit){
+
+    visorEL.innerHTML += _digit;
+}
